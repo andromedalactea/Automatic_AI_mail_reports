9b16d56cf2db0cf8fc519bb5351d01708794ec42
andromedalactea <bry3639@gmail.com>
2024-10-01 14:23:32 -0500

    [REAFCTOR] name of file with date

--- scripts/main.py
+++ scripts/main.py
@@ -19,6 +19,9 @@ pd.set_option('display.max_columns', None)
 # Define the main function
 def main_automatic_report():
     try:
+        # If not exist files directory create it
+        os.makedirs(absolute_path('../files'), exist_ok=True)
+
         # Extract the timing information
         current_time_ny, start_of_day_ny = get_ny_time_and_start_of_day()
         
@@ -38,15 +41,16 @@ def main_automatic_report():
         # Calificate the calls
         else:
             print(len(leads_report))
-            # leads_report = leads_report.head(2) # DELETE THIS LINE IN PRODUCTION
+            # leads_report = leads_report.head(1) # DELETE THIS LINE IN PRODUCTION
             leads_report = calificate_calls_from_df(leads_report)
 
         # Name for the pdf
-        pdf_name = os.getenv("ATTACHMENT_NAME") + ".pdf"
+        pdf_name = f"{os.getenv('ATTACHMENT_NAME')}_{current_time_ny.split(' ')[0]}.pdf"
+        pdf_path = absolute_path(f"../files/{pdf_name}")
 
         # Convert the DataFrame to a PDF
         css_path = absolute_path('../css/style_report.css')
-        df_to_pdf(leads_report, pdf_name, css_path)
+        df_to_pdf(leads_report, pdf_path, css_path)
 
         ## Send the email with the attachment
         sender_email = os.getenv("SENDER_EMAIL")
@@ -58,7 +62,7 @@ def main_automatic_report():
         body = "This email contains an attached PDF with today's lead reports."
 
         for receiver_email in receiver_email_list:
-            send_email_with_attachment(sender_email, receiver_email, subject, body, pdf_name)
+            send_email_with_attachment(sender_email, receiver_email, subject, body, pdf_path)
 
         # # Print the first 5 rows of the DataFrame
         # leads_report.to_csv('calificate_calls.csv', index=False)
